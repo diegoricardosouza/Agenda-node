3dd43263983cd3e57e65c1980a6682044fdf1e1c
Diego Ricardo <diegoricardoweb@gmail.com>
2020-10-08 21:13:45 -0300

    create users

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,20 +1,21 @@
 exports.middlewareGlobal = (req, res, next) => {
-  res.locals.umaVariavelLocal = 'Este é o valor da variável local.';
-  next();
+    res.locals.errors = req.flash('errors');
+    res.locals.success = req.flash('success');
+    next();
 };
 
 exports.outroMiddleware = (req, res, next) => {
-  next();
+    next();
 };
 
 exports.checkCsrfError = (err, req, res, next) => {
-  if(err) {
-    return res.render('404');
-  }
-  next();
+    if (err) {
+        return res.render('404');
+    }
+    next();
 };
 
 exports.csrfMiddleware = (req, res, next) => {
-  res.locals.csrfToken = req.csrfToken();
-  next();
+    res.locals.csrfToken = req.csrfToken();
+    next();
 };
