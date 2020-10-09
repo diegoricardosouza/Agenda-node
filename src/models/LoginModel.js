eda6bf55c94c41ba66344ef7a5ac305743235749
Diego Ricardo <diegoricardoweb@gmail.com>
2020-10-08 22:33:00 -0300

    logging in user

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -16,6 +16,23 @@ class Login {
         this.user = null;
     }
 
+    async login() {
+        this.valida();
+        if (this.errors.length > 0) return;
+        this.user = await LoginModel.findOne({ email: this.body.email });
+
+        if(!this.user) {
+            this.errors.push('usuário não existe.');
+            return;
+        }
+
+        if(!bcryptjs.compareSync(this.body.password, this.user.password)) {
+            this.errors.push('Senha inválida');
+            this.user = null;
+            return;
+        }
+    }
+
     async register() {
         this.valida();
         if (this.errors.length > 0) return;
@@ -31,8 +48,8 @@ class Login {
     }
 
     async userExists () {
-        const user = await LoginModel.findOne({email: this.body.email});
-        if(user) this.errors.push('Usuário já existe.');
+        this.user = await LoginModel.findOne({email: this.body.email});
+        if(this.user) this.errors.push('Usuário já existe.');
     }
 
     valida() {
