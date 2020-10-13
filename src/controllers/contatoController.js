047fb4115eca43dea83067760bf5b668eeb876c7
Diego Ricardo <diegoricardoweb@gmail.com>
2020-10-13 20:13:36 -0300

    edit contacts

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -1,3 +1,4 @@
+const { async } = require('regenerator-runtime');
 const Contato = require('../models/ContatoModel');
 
 exports.index = (req, res) => {
@@ -32,4 +33,25 @@ exports.editIndex = async (req, res) => {
     if(!contato) return res.render('404');
 
     res.render('contato', { contato });
+}
+
+exports.edit = async (req, res) => {
+    try {
+        if (!req.params.id) return res.render('404');
+        const contato = new Contato(req.body);
+        await contato.edit(req.params.id);
+
+        if (contato.errors.length > 0) {
+            req.flash('errors', contato.errors);
+            req.session.save(() => res.redirect('back'));
+            return;
+        }
+
+        req.flash('success', 'Contato editado com sucesso.');
+        req.session.save(() => res.redirect(`/contato/${contato.contato._id}`));
+        return;
+    } catch (e) {
+        console.log(e);
+        return res.render('404');
+    }
 }
